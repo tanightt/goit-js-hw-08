dac53c77876b349cbc1c28db2c9668a087970020
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-05-26 21:51:18 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+onCreateGalleryCards();
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+
+function onCreateGalleryCards() {
+    let galleryCards = '';
+
+    galleryItems.forEach(item => {
+        const galleryCard = `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+        </a>
+        </li>`
+    galleryCards += galleryCard;
+}); 
+    galleryList.insertAdjacentHTML("afterbegin", galleryCards);
+}
