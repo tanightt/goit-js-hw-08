3bbf4a507466fd780438965703e05dc977d9ac08
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-05-27 18:58:16 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+const onPlay = function (data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data)); 
+};
+player.on('timeupdate', throttle(onPlay, 2000));
+
+const timeKey = JSON.parse(localStorage.getItem(STORAGE_KEY));
+startTimeVideo();
+function startTimeVideo() {
+    if (timeKey === null) {
+        return;
+    } 
+    player.setCurrentTime(timeKey['seconds']); 
+}
